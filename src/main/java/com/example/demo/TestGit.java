0daed9730bcdfd58c865b79a6cb4eb8183d6c323
haitan123 <66637260+haitan123@users.noreply.github.com>
2020-10-25 21:42:23 +0700

    Update TestGit.java

--- src/main/java/com/example/demo/TestGit.java
+++ src/main/java/com/example/demo/TestGit.java
@@ -2,6 +2,6 @@ package com.example.demo;
 
 public class TestGit {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("Hi hai tan");
     }
 }
